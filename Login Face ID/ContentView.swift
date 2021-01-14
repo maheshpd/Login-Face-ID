e458a9bf76d4c8b62ee5030337946d5b44709480
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-14 17:42:25 +0530

    Initial Commit

---
+++ Login Face ID/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login Face ID
+//
+//  Created by Mahesh Prasad on 14/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Login Face ID/Login_Face_IDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_Face_IDApp.swift
+//  Login Face ID
+//
+//  Created by Mahesh Prasad on 14/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Login_Face_IDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
